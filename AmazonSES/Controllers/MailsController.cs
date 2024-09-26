99758424dfccbff8b06873135723b2fc3673c5f4
eliseo-tmp <eliseo@primetix.com>
2024-09-26 11:32:34 -0600

    Carga de codigo

---
+++ AmazonSES/Controllers/MailsController.cs
@@ -0,0 +1,23 @@
+﻿using AmazonSESTest.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmazonSESTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailsController : ControllerBase
+    {
+        private readonly MailService _mailService;
+        public MailsController(MailService mailService)
+        {
+            this._mailService = mailService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMail(MailRequest request)
+        {
+            await _mailService.SendEmailAsync(request);
+            return Ok();
+        }
+    }
+}
---
+++ AmazonSES/Services/MailService.cs
@@ -0,0 +1,35 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace AmazonSESTest.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+
+    public class MailService : IMailService
+    {
+        private readonly MailSettings _mailSettings;
+        public MailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+        public async Task SendEmailAsync(MailRequest mailRequest)
+        {
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress(_mailSettings.DisplayName, _mailSettings.Mail));
+            email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+            email.Subject = mailRequest.Subject;
+            var builder = new BodyBuilder();
+            builder.HtmlBody = mailRequest.Body;
+            email.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, MailKit.Security.SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Username, _mailSettings.Password);
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
